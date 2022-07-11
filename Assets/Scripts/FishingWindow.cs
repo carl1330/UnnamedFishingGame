6d722ff31a5f83e20a93f51737aa352f56e082a9
Carl Gulliksson <gulliksson.carl@gmail.com>
2022-07-12 00:19:37 +0200

    test

--- Assets/Scripts/FishingWindow.cs
+++ Assets/Scripts/FishingWindow.cs
@@ -19,5 +19,6 @@ public class FishingWindow : MonoBehaviour
         GameObject canvas = GameObject.Find("TestCanvas");
         cam.targetTexture = test;
         canvas.transform.position = new Vector3(main.transform.position.x + 2, main.transform.position.y - 2.5f, 0);   
+
     }
 }
