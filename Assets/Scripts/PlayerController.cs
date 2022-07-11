0c294c28c58b4157e61640a9b8cb79e4ac4e1118
Carl Gulliksson <dv21cgn@cs.umu.se>
2022-07-11 17:00:33 +0200

    created simple pokemon like controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,47 +1,40 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 2f;
+    public Transform movePoint;
+    private bool movingHori = false;
+    private bool movingVert = false;
 
-    public GameObject FishingRod;
-    Rigidbody2D RB;
-    float firstPress;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePoint.parent = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        ChargePower();
-        
-    }
+        //Checks if player is running or not
+        if(!Input.GetKey(KeyCode.LeftShift))
+            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        else
+            transform.position = Vector3.MoveTowards(transform.position, movePoint.position, 1.5f * moveSpeed * Time.deltaTime);
 
-    void ChargePower() {
-
-        float timePressed;
-        if(Input.GetKeyDown("space")) {
-            firstPress = Time.time;
-        }
-        if (Input.GetKeyUp("space"))
+        //If the player has reached movePoint, movePoint is then allowed to move.
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            timePressed = Time.time - firstPress;
-            CastFishingRod(timePressed);
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);       
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            {
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
         }
     }
-
-    void CastFishingRod(float t)
-    {
-        //Throw
-        float power = 5 * t;
-
-        Vector2 Force = new Vector2(power, power);
-        FishingRod.GetComponent<Rigidbody2D>().AddForce(Force, ForceMode2D.Impulse);
-    }
-
 }
