e002940e8ab27ae1e82749919778300130aaa845
simonmonkeyz <simonmonkey99@gmail.com>
2022-07-13 14:51:42 +0200

    added casting with lmb
    just add the SpinFishing script to the player to make it work

--- Assets/Scripts/SpinFishing.cs
+++ Assets/Scripts/SpinFishing.cs
@@ -2,28 +2,87 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SpinFishing : MonoBehaviour
 {
+    private IEnumerator coroutine;
+
+
     bool isFishing;
+    bool isCasting;
+    float balanceIndex;
+    float power;
+    const float max=10;
+    const float min=0.1f;
+    bool chargeCast;
     // Start is called before the first frame update
     void Start()
     {
-        
+        coroutine = WaitAndPrint(0.01f);
+    }
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            if(isCasting==false)
+            {
+
+                isCasting = true;
+              
+                power = 0;
+                StartCoroutine(WaitAndPrint(0.005f));
+            }
+
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            isCasting = false;
+            //StopCoroutine(StartCoroutine(WaitAndPrint(0.01f)));
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void fishingGame()
     {
-
-        if (isFishing)
+        while(isFishing)
         {
 
 
 
+        }
+    }
 
+  
 
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (isCasting)
+        {
+            if(power>=10f)
+            {
+                chargeCast = false;
 
+            }
+            else if(power<=0.0f)
+            {
+                chargeCast = true;
+            }
+            if(chargeCast)
+            {
+                power += 0.1f;
+            }
+            else if(!chargeCast)
+            {
+                power -= 0.1f;
+            }
+         
+                yield return new WaitForSeconds(waitTime);
+                print("Power: " + power);
+            
+          
 
+            
+        
         }
     }
 }
