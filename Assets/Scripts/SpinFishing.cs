95cc9e249066d45fe50eac2680ff9b6fdeecf281
Carl Gulliksson <gulliksson.carl@gmail.com>
2022-07-14 12:39:40 +0200

    formatting fix

--- Assets/Scripts/SpinFishing.cs
+++ Assets/Scripts/SpinFishing.cs
@@ -24,7 +24,7 @@ public class SpinFishing : MonoBehaviour
         if(Input.GetMouseButtonDown(0))
         {
 
-            if(isCasting==false)
+            if(!isCasting)
             {
 
                 isCasting = true;
