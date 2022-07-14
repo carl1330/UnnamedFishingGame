07f328444820a81c9d8f380ee37e0e930183b6c2
simonmonkeyz <simonmonkey99@gmail.com>
2022-07-14 20:05:18 +0200

    SpinFishing

--- Assets/Scripts/SpinFishing.cs
+++ Assets/Scripts/SpinFishing.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SpinFishing : MonoBehaviour
@@ -14,19 +15,26 @@ public class SpinFishing : MonoBehaviour
     const float max=10;
     const float min=0.1f;
     bool chargeCast;
+    Image powerBar;
+    GameObject powerbar;
     // Start is called before the first frame update
     void Start()
     {
+        
+        powerBar = GameObject.Find("bar").GetComponent<Image>();
         coroutine = WaitAndPrint(0.01f);
+        powerbar = GameObject.Find("PowerBar");
+        powerbar.SetActive(false);
     }
     private void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
+            StopAllCoroutines();
 
             if(isCasting==false)
             {
-
+                powerbar.SetActive(true);
                 isCasting = true;
               
                 power = 0;
@@ -36,7 +44,10 @@ public class SpinFishing : MonoBehaviour
         }
         if(Input.GetMouseButtonUp(0))
         {
+            
             isCasting = false;
+            StartCoroutine(Delay(2.0f));
+           
             //StopCoroutine(StartCoroutine(WaitAndPrint(0.01f)));
 
         }
@@ -77,6 +88,7 @@ public class SpinFishing : MonoBehaviour
             }
          
                 yield return new WaitForSeconds(waitTime);
+            powerBar.fillAmount = power * 0.1f;
                 print("Power: " + power);
             
           
@@ -84,5 +96,13 @@ public class SpinFishing : MonoBehaviour
             
         
         }
+    }
+    IEnumerator Delay(float time)
+    {
+        
+
+        yield return new WaitForSeconds(time);
+        powerbar.SetActive(false);
+        // Code to execute after the delay
     }
 }
